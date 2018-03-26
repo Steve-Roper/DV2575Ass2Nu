549b8be4eeaf82b686ded35a597495046f89aa3d
niwc13 <niwc13@H473-0004>
2018-03-26 10:06:54 +0200

    malloc and memcpy for dev

--- Ass2/kernel.cu
+++ Ass2/kernel.cu
@@ -37,6 +37,7 @@ int main()
 	vector<vector<float>> data = { { 8,5,7 },{ 4,6,3 },{ 3,1,9 } };
 	vector<float> vector = { 2,5,3 };
 	std::vector<std::vector<float>> data2;
+	data2 = data;
 	data2[0].push_back(2);
 	data2[1].push_back(5);
 	data2[2].push_back(3);
@@ -103,7 +104,7 @@ cudaError_t addWithCuda(int *c, const int *a, const int *b, unsigned int size)
     }
 
     // Launch a kernel on the GPU with one thread for each element.
-    addKernel<<<1, size>>>(dev_c, dev_a, dev_b);
+    //addKernel<<<1, size>>>(dev_c, dev_a, dev_b);
 
     // Check for any errors launching the kernel
     cudaStatus = cudaGetLastError();
@@ -138,7 +139,7 @@ Error:
 void Gaussian(vector<vector<float>> &data, std::vector<float> &vector)
 {
 	ForwardElim(data, vector);
-	BackSub(data, vector);
+	//BackSub(data, vector);
 }
 
 void ForwardElim(vector<vector<float>> &data, std::vector<float> &vector)
@@ -178,18 +179,106 @@ void BackSub(vector<vector<float>> &data, std::vector<float> &vector)
 
 void GPUGaussian(vector<vector<float>>& data, int size)
 {
-	KernelForwardElim<<<1,4>>>()
-}
+	float* devUpperRow		= 0;
+	float* devLowerRow		= 0;
+	int* devSize			= 0;
+	float* devMultiplier	= 0;
+	int* devUpperRowIdx		= 0;
 
-__global__ void KernelForwardElim(float* upper, float* lower, int* _size, float* multiplier, int* _upperRow)
+	//malloc rows
+	cudaError_t cudaStatus = cudaMalloc((void**)&devUpperRow, (size + 1) * sizeof(float));
+	if (cudaStatus != cudaSuccess) {
+		fprintf(stderr, "cudaMalloc failed for upperRow\n");
+		return;
+	}
+	cudaStatus = cudaMalloc((void**)&devLowerRow, (size + 1) * sizeof(float));
+	if (cudaStatus != cudaSuccess) {
+		fprintf(stderr, "cudaMalloc failed for lowerRow\n");
+		return;
+	}
+	//malloc rest
+	cudaStatus = cudaMalloc((void**)&devSize, sizeof(int));
+	if (cudaStatus != cudaSuccess) {
+		fprintf(stderr, "cudaMalloc failed for size\n");
+		return;
+	}
+	cudaStatus = cudaMalloc((void**)&devMultiplier, sizeof(float));
+	if (cudaStatus != cudaSuccess) {
+		fprintf(stderr, "cudaMalloc failed for multiplier\n");
+		return;
+	}
+	cudaStatus = cudaMalloc((void**)&devUpperRowIdx, sizeof(int));
+	if (cudaStatus != cudaSuccess) {
+		fprintf(stderr, "cudaMalloc failed for upperRowIdx\n");
+		return;
+	}
+
+	//memcpy size
+	cudaStatus = cudaMemcpy(devSize, &size, sizeof(int), cudaMemcpyHostToDevice);
+	if (cudaStatus != cudaSuccess) {
+		fprintf(stderr, "cudaMemcpy failed for size\n");
+		return;
+	}
+
+	for (int i = 0; i < (size - 1); ++i)
+	{
+		//for a given pivot row, reduce all items below to zero
+		for (int j = i + 1; j < (size - 1); ++j)
+		{
+			float multiplier = data[i][i] / data[j][i];
+			cudaStatus = cudaMemcpy(devUpperRow, data[i].data(), (size + 1) * sizeof(float), cudaMemcpyHostToDevice);
+			if (cudaStatus != cudaSuccess) {
+				fprintf(stderr, "cudaMemcpy failed for upperRow\n");
+				return;
+			}
+			cudaStatus = cudaMemcpy(devLowerRow, data[j].data(), (size + 1) * sizeof(float), cudaMemcpyHostToDevice);
+			if (cudaStatus != cudaSuccess) {
+				fprintf(stderr, "cudaMemcpy failed for lowerRow\n");
+				return;
+			}
+			cudaStatus = cudaMemcpy(devMultiplier, &multiplier, sizeof(float), cudaMemcpyHostToDevice);
+			if (cudaStatus != cudaSuccess) {
+				fprintf(stderr, "cudaMemcpy failed for multiplier\n");
+				return;
+			}
+			cudaStatus = cudaMemcpy(devUpperRowIdx, &i, sizeof(int), cudaMemcpyHostToDevice);
+			if (cudaStatus != cudaSuccess) {
+				fprintf(stderr, "cudaMemcpy failed for multiplier\n");
+				return;
+			}
+
+			KernelForwardElim<<<1, 4>>>(devUpperRow, devLowerRow, devSize, devMultiplier, devUpperRowIdx);
+
+			float* upperRow = 0;
+			float* lowerRow = 0;
+
+			cudaStatus = cudaMemcpy(upperRow, devUpperRow, (size + 1) * sizeof(float), cudaMemcpyDeviceToHost);
+			if (cudaStatus != cudaSuccess) {
+				fprintf(stderr, "cudaMemcpy failed for upperRow\n");
+				return;
+			}
+			cudaStatus = cudaMemcpy(lowerRow, devLowerRow, (size + 1) * sizeof(float), cudaMemcpyDeviceToHost);
+			if (cudaStatus != cudaSuccess) {
+				fprintf(stderr, "cudaMemcpy failed for lowerRow\n");
+				return;
+			}
+
+			int stopper = 0;
+		}
+	}
+
+
+}
+//											data[i],	data[j],	data.size(), upperMainDiag/theOneUnderIt 
+__global__ void KernelForwardElim(float* upperRow, float* lowerRow, int* _size, float* multiplier, int* _upperRowIdx)
 {
 	int col = threadIdx.x + blockIdx.x * blockDim.x;
-	int upperRow = *_upperRow;
-	if (col >= upperRow)
+	int upperRowIdx = *_upperRowIdx;
+	if (col >= upperRowIdx)
 	{
 		int size = *_size;
-		lower[col] *= *multiplier;
-		lower[col] -= upper[col];
+		lowerRow[col] *= *multiplier;
+		lowerRow[col] -= upperRow[col];
 	}
 }
 
