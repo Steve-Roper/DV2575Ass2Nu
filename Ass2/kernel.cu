b070ad8a15aadf2e24b66f844aafc74347e8605d
livc15 <linnea.vajda@live.se>
2018-05-16 16:13:29 +0200

    Stride bugged

--- Ass2/kernel.cu
+++ Ass2/kernel.cu
@@ -4,7 +4,7 @@
 #include <stdio.h>
 #include <vector>
 #include <iostream>
-#define epsilon 0.00000001
+#define epsilon 0.000001
 
 using namespace std;
 
@@ -13,16 +13,11 @@ void Gaussian(vector<vector<double>> &data, int size);
 void ForwardElim(vector<vector<double>> &data, int size);
 void BackSub(vector<vector<double>> &data, int size);
 void SwapRows(vector<vector<double>> &data, int size, int upperRow, int lowerRow);
-void CompareResults(vector<vector<double>> &data, vector<vector<double>> &data2, int size);
+bool CompareResults(vector<vector<double>> &data, vector<vector<double>> &data2, int size);
 
-void GPUGaussian(vector<vector<double>> &data, int size);
-__global__ void KernelForwardElim(double* upper, double* lower, int* _size, double* multiplier, int* _upperRow);
+void GPUGaussian(vector<vector<double>> &data, int size, int blocks, int colsPerThread);
+__global__ void KernelForwardElim(double* upper, double* lower, int* _size, double* multiplier, int* _upperRow, int* colsPerThread);
 
-//__global__ void addKernel(int *c, const int *a, const int *b)
-//{
-//    int i = threadIdx.x;
-//    c[i] = a[i] + b[i];
-//}
 
 void FillMatrix(int size, vector<vector<double>> &data);
 
@@ -31,29 +26,29 @@ int main()
 	srand(time(NULL));
     // Add vectors in parallel.
 	cudaError_t cudaStatus = cudaSuccess;
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "addWithCuda failed!");
-        return 1;
-    }
 
-	int size = 15;
-	int nrOfThreads = 1;
-	//vector<vector<double>> data = { { 8,5,7 },{ 4,6,3 },{ 3,1,9 } };
-	//vector<double> vector = { 2,5,3 };
-	vector<vector<double>> data, data2;
-	//data.reserve(size * size + 1);
-	//data2.reserve(size * size + 1);
-	//data2 = data;
-	//data2[0].push_back(2);
-	//data2[1].push_back(5);
-	//data2[2].push_back(3);
-	//data = data2;
-	FillMatrix(size, data);
-	data2 = data;
-	Gaussian(data, size);
-	GPUGaussian(data2, size);
-	CompareResults(data, data2, size);
-	
+	//int size = 256;
+	//int colsPerThread = 1;
+	vector<vector<double>> data, data2, backup;
+
+	for (int size = 3; size < 5; size *= 2)
+	{
+		FillMatrix(size, data);
+		backup = data;
+		Gaussian(data, size);
+		for (int colsPerThread = 1; colsPerThread < 9; colsPerThread *= 2)
+		{
+			data2.clear();
+			data2 = backup;
+			int threads = (size + 1) / colsPerThread;
+			int blocks = (threads - 1) / 1024 + 1; /*1024 max for current graphics card used*/
+			GPUGaussian(data2, size, blocks, colsPerThread);
+			if (!CompareResults(data, data2, size))
+			{
+				break;
+			}
+		}
+	}
 	// cudaDeviceReset must be called before exiting in order for profiling and
     // tracing tools such as Nsight and Visual Profiler to show complete traces.
     cudaStatus = cudaDeviceReset();
@@ -62,94 +57,19 @@ int main()
         return 1;
     }
 
-
 	cin.get();
     return 0;
 }
 
-// Helper function for using CUDA to add vectors in parallel.
-cudaError_t addWithCuda(int *c, const int *a, const int *b, unsigned int size)
-{
-    int *dev_a = 0;
-    int *dev_b = 0;
-    int *dev_c = 0;
-    cudaError_t cudaStatus;
-
-    // Choose which GPU to run on, change this on a multi-GPU system.
-    cudaStatus = cudaSetDevice(0);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaSetDevice failed!  Do you have a CUDA-capable GPU installed?");
-        goto Error;
-    }
-
-    // Allocate GPU buffers for three vectors (two input, one output)    .
-    cudaStatus = cudaMalloc((void**)&dev_c, size * sizeof(int));
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMalloc failed!");
-        goto Error;
-    }
-
-    cudaStatus = cudaMalloc((void**)&dev_a, size * sizeof(int));
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMalloc failed!");
-        goto Error;
-    }
-
-    cudaStatus = cudaMalloc((void**)&dev_b, size * sizeof(int));
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMalloc failed!");
-        goto Error;
-    }
-
-    // Copy input vectors from host memory to GPU buffers.
-    cudaStatus = cudaMemcpy(dev_a, a, size * sizeof(int), cudaMemcpyHostToDevice);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMemcpy failed!");
-        goto Error;
-    }
-
-    cudaStatus = cudaMemcpy(dev_b, b, size * sizeof(int), cudaMemcpyHostToDevice);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMemcpy failed!");
-        goto Error;
-    }
-
-    // Launch a kernel on the GPU with one thread for each element.
-    //addKernel<<<1, size>>>(dev_c, dev_a, dev_b);
-
-    // Check for any errors launching the kernel
-    cudaStatus = cudaGetLastError();
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "addKernel launch failed: %s\n", cudaGetErrorString(cudaStatus));
-        goto Error;
-    }
-    
-    // cudaDeviceSynchronize waits for the kernel to finish, and returns
-    // any errors encountered during the launch.
-    cudaStatus = cudaDeviceSynchronize();
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaDeviceSynchronize returned error code %d after launching addKernel!\n", cudaStatus);
-        goto Error;
-    }
-
-    // Copy output vector from GPU buffer to host memory.
-    cudaStatus = cudaMemcpy(c, dev_c, size * sizeof(int), cudaMemcpyDeviceToHost);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMemcpy failed!");
-        goto Error;
-    }
-
-Error:
-    cudaFree(dev_c);
-    cudaFree(dev_a);
-    cudaFree(dev_b);
-    
-    return cudaStatus;
-}
-
 void Gaussian(vector<vector<double>> &data, int size)
 {
+	clock_t t;
+	t = clock();
 	ForwardElim(data, size);
+	t = clock() - t;
+
+	std::cout << "CPU Forward Substituion took: " << t << "clicks ("<< ((float)t)/CLOCKS_PER_SEC << " seconds.)" << endl;
+
 	BackSub(data, size);
 }
 
@@ -191,7 +111,8 @@ void ForwardElim(vector<vector<double>> &data, int size)
 			}
 			double lower = data[j][i];
 			double multiplier = upper / lower;
-			for (unsigned int k = 0; k < size + 1; ++k)
+			data[j][i] = 0;
+			for (unsigned int k = i; k < size + 1; ++k)
 			{
 				data[j][k] *= multiplier;
 				data[j][k] -= data[i][k];
@@ -215,13 +136,14 @@ void BackSub(vector<vector<double>> &data, int size)
 	}
 }
 
-void GPUGaussian(vector<vector<double>>& data, int size)
+void GPUGaussian(vector<vector<double>>& data, int size, int blocks, int colsPerThread)
 {
 	double* devUpperRow		= 0;
 	double* devLowerRow		= 0;
 	int* devSize			= 0;
 	double* devMultiplier	= 0;
 	int* devUpperRowIdx		= 0;
+	int* devColsPerThread	= 0;
 
 	//malloc rows
 	cudaError_t cudaStatus = cudaMalloc((void**)&devUpperRow, (size + 1) * sizeof(double));
@@ -250,6 +172,11 @@ void GPUGaussian(vector<vector<double>>& data, int size)
 		fprintf(stderr, "cudaMalloc failed for upperRowIdx\n");
 		return;
 	}
+	cudaStatus = cudaMalloc((void**)&devColsPerThread, sizeof(int));
+	if (cudaStatus != cudaSuccess) {
+		fprintf(stderr, "cudaMalloc failed for colsPerThread\n");
+		return;
+	}
 
 	//memcpy size
 	cudaStatus = cudaMemcpy(devSize, &size, sizeof(int), cudaMemcpyHostToDevice);
@@ -257,7 +184,14 @@ void GPUGaussian(vector<vector<double>>& data, int size)
 		fprintf(stderr, "cudaMemcpy failed for size\n");
 		return;
 	}
+	cudaStatus = cudaMemcpy(devColsPerThread, &colsPerThread, sizeof(int), cudaMemcpyHostToDevice);
+	if (cudaStatus != cudaSuccess) {
+		fprintf(stderr, "cudaMemcpy failed for colsPerThread\n");
+		return;
+	}
 
+	clock_t t;
+	t = clock();
 	for (int i = 0; i < (size - 1); ++i)
 	{
 		if (abs(data[i][i]) < epsilon)
@@ -318,7 +252,8 @@ void GPUGaussian(vector<vector<double>>& data, int size)
 				return;
 			}
 
-			KernelForwardElim<<<1, size + 1>>>(devUpperRow, devLowerRow, devSize, devMultiplier, devUpperRowIdx);
+			KernelForwardElim<<<blocks, 1024>>>(devUpperRow, devLowerRow, devSize, devMultiplier, devUpperRowIdx, devColsPerThread);
+			
 
 			double* lowerRow = (double*)malloc((size + 1) * sizeof(double));
 
@@ -332,8 +267,10 @@ void GPUGaussian(vector<vector<double>>& data, int size)
 			{
 				data[j][k] = lowerRow[k];
 			}
-		}
+		}	
 	}
+	t = clock() - t;
+	std::cout << "GPU Forward Substituion took: " << t << "clicks (" << ((float)t) / CLOCKS_PER_SEC << " seconds.)" << endl;
 	BackSub(data, size);
 }
 
@@ -345,17 +282,17 @@ void SwapRows(vector<vector<double>> &data, int size, int upperRow, int lowerRow
 	data[lowerRow] = temp;
 }
 
-void CompareResults(vector<vector<double>>& data, vector<vector<double>>& data2, int size)
+bool CompareResults(vector<vector<double>>& data, vector<vector<double>>& data2, int size)
 {
 	bool test = true;
 	for (int i = 0; i < size; ++i)
 	{
 		for (int j = 0; j < size + 1; ++j)
 		{
-			if (abs(data[i][j] - data2[i][j]) > epsilon)
+			if (abs(data[i][j] - data2[i][j]) > epsilon && abs(data[i][j]) > epsilon && abs(data2[i][j]) > epsilon)
 			{
 				cout << "Something went wrong" << endl;
-				cout << "CPU: " << data[i][j] << "|\t" << data2[i][j] << endl;
+				cout << "CPU: " << data[i][j] << "|\tGPU:" << data2[i][j] << endl;
 				test = false;
 			}
 		}
@@ -364,17 +301,26 @@ void CompareResults(vector<vector<double>>& data, vector<vector<double>>& data2,
 	{
 		cout << "CPU and GPU results match!" << endl;
 	}
+	return test;
 }
 
 //											data[i],	data[j],	data.size(), upperMainDiag/theOneUnderIt 
-__global__ void KernelForwardElim(double* upperRow, double* lowerRow, int* _size, double* multiplier, int* _upperRowIdx)
+__global__ void KernelForwardElim(double* upperRow, double* lowerRow, int* _size, double* multiplier, int* _upperRowIdx, int* colsPerThread)
 {
-	int col = threadIdx.x + blockIdx.x * blockDim.x;
+	int _colsPerThread = *colsPerThread;
+	int startCol = threadIdx.x + blockIdx.x * blockDim.x * _colsPerThread;
 	int upperRowIdx = *_upperRowIdx;
-	if (col >= upperRowIdx && col <= *_size)
+	for (int col = startCol; col < _colsPerThread + startCol; ++col)
 	{
-		lowerRow[col] *= *multiplier;
-		lowerRow[col] -= upperRow[col];
+		if (col > upperRowIdx && col <= *_size)
+		{
+			lowerRow[col] *= *multiplier;
+			lowerRow[col] -= upperRow[col];
+		}
+		if (col == upperRowIdx)
+		{
+			lowerRow[col] = 0;
+		}
 	}
 }
 
